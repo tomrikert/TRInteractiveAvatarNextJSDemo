d0246b0066289791384ea33496410b5c260f2627
Tom Rikert <tomrikert@gmail.com>
2024-11-22 20:54:31 -0800

    Cut and paste from Vercel AI SDK

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,17 @@
-import { streamText } from "ai";
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
-/* export const maxDuration = 30;
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const result = await streamText({
-    model: "gpt-4",
+  const result = streamText({
+    model: openai('gpt-4-turbo'),
+    system: 'You are a helpful assistant.',
     messages,
   });
 
-  return result.toAIStreamResponse();
+  return result.toDataStreamResponse();
 }
-*/
